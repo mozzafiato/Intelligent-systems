f515b5011dc94209210036c4b39460af28863e52
melanijak <kraljevskamelanija@gmail.com>
2019-12-24 22:06:59 +0100

    Izboljsave 1. in 2. del

--- solution.R
+++ solution.R
@@ -1,3 +1,4 @@
+options(java.parameters = "- Xmx1024m")
 library(ggplot2)
 library(tm)
 library(readr)
@@ -11,104 +12,112 @@ library(useful)
 library(kernlab)
 library(class)
 library(e1071)
+library(textclean)
+library(factoextra)
 
-# setwd("C:/Users/Melanija/Desktop/Gradiva/5 semestar/IS/2. Seminarska")
-setwd("C:/Users/Jana/Documents/fax/3.letnik/1.semester/IS/2.domaca/")
+ setwd("C:/Users/Melanija/Desktop/Gradiva/5 semestar/IS/2. Seminarska")
+#setwd("C:/Users/Jana/Documents/fax/3.letnik/1.semester/IS/2.domaca/")
 
 train <- read.table(file = 'insults/train.tsv', sep = '\t', header = TRUE)
 test <- read.table(file = 'insults/test.tsv', sep = '\t', header = TRUE)
 
 # 1.Cleaning
-
-#Remove punctuation and stopwords
-corpus <- Corpus(VectorSource(train$text_a))
-corpus <- tm_map(corpus, removeWords, stopwords('english'))
-conn = file("english.stop.txt", open="r")
-mystopwords = readLines(conn)
-close(conn)
-#Remove unknown symbols
-corpus <- tm_map(corpus, removeWords, mystopwords)
-conn = file("unknownWords.txt", open="r")
-unknown = readLines(conn)
-close(conn)
-corpus <- tm_map(corpus, removeWords, unknown)
-corpus <- tm_map(corpus, removeNumbers)
-corpus <- tm_map(corpus, removePunctuation)
-corpus <- tm_map(corpus, stripWhitespace)
-length(corpus)
-
-#Anonymize proper nouns
-sent_ann <- Maxent_Sent_Token_Annotator()
-word_ann <- Maxent_Word_Token_Annotator() 
-person_ann <- Maxent_Entity_Annotator(kind = "person")
-location_ann <- Maxent_Entity_Annotator(kind = "location")
-organization_ann <- Maxent_Entity_Annotator(kind = "organization")
-
-entities <- function(annots, kind)
-{
-  k <- sapply(annots$features, `[[`, "kind")
-  s[annots[k == kind]]
+clean_data <- function (data) {
+  #Remove unknown symbols
+  data$text_a <- sapply(data$text_a, function(x) gsub("x[[:alnum:]][[:digit:]]", "", as.character(x)))
+  data$text_a <- sapply(data$text_a, function(x) gsub("\\n", "", as.character(x)))
+  data$text_a <- sapply(data$text_a, function(x) gsub("\\r", "", as.character(x)))
+  data$text_a <- sapply(data$text_a, function(x) gsub("\\t", "", as.character(x)))
+  data$text_a <- sapply(data$text_a, function(x) gsub("\\u[[:digit:]][[:digit:]][[:digit:]][[:alnum:]]", "", as.character(x)))
+  
+  corpus <- Corpus(VectorSource(data$text_a))
+  
+  #Remove punctuation and stopwords
+  corpus <- tm_map(corpus, removeWords, stopwords('english'))
+  conn = file("english.stop.txt", open="r")
+  mystopwords = readLines(conn)
+  close(conn)
+  corpus <- tm_map(corpus, removeWords, mystopwords)
+  corpus <- tm_map(corpus, removeNumbers)
+  corpus <- tm_map(corpus, removePunctuation)
+  corpus <- tm_map(corpus, stripWhitespace)
+  
+  #Anonymize proper nouns
+  sent_ann <- Maxent_Sent_Token_Annotator()
+  word_ann <- Maxent_Word_Token_Annotator() 
+  person_ann <- Maxent_Entity_Annotator(kind = "person")
+  location_ann <- Maxent_Entity_Annotator(kind = "location")
+  organization_ann <- Maxent_Entity_Annotator(kind = "organization")
+  
+  entities <- function(annots, kind)
+  {
+    k <- sapply(annots$features, `[[`, "kind")
+    s[annots[k == kind]]
+  }
+  
+  for (i in 1:length(corpus)){
+    s <- as.String(content(corpus[[i]]))
+    if(nchar(trimws(s)) == 0) next
+    
+    ann <- annotate(s, list(sent_ann, word_ann, person_ann, location_ann, organization_ann))
+    #print(ann)
+    #print(entities(ann, "person"))
+    #print(entities(ann, "location"))
+    #print(entities(ann, "organization"))
+    
+    corpus <- tm_map(corpus, removeWords, as.vector(entities(ann, "person")))
+    corpus <- tm_map(corpus, removeWords, as.vector(entities(ann, "location")))
+    corpus <- tm_map(corpus, removeWords, as.vector(entities(ann, "organization")))
+    content(corpus[[i]])
+  }
+  corpus <- tm_map(corpus, stripWhitespace)
+  corpus <- tm_map(corpus, content_transformer(tolower))
+  corpus <- tm_map(corpus, stemDocument)
+  corpus
 }
 
-for (i in 1:length(corpus)){
-  s <- as.String(content(corpus[[i]]))
-  if(nchar(trimws(s)) == 0) next
-  
-  ann <- annotate(s, list(sent_ann, word_ann, person_ann, location_ann, organization_ann))
-  #print(ann)
-  #print(entities(ann, "person"))
-  #print(entities(ann, "location"))
-  #print(entities(ann, "organization"))
-  
-  corpus <- tm_map(corpus, removeWords, as.vector(entities(ann, "person")))
-  corpus <- tm_map(corpus, removeWords, as.vector(entities(ann, "location")))
-  corpus <- tm_map(corpus, removeWords, as.vector(entities(ann, "organization")))
-  content(corpus[[i]])
-}
-
+train_corpus <- clean_data(train)
+content(train_corpus[[1]])
 
 # 2.Exploration
 
 #Plot the frequency of words
-corpus <- tm_map(corpus, content_transformer(tolower))
-corpus <- tm_map(corpus, stemDocument)
 tdm <- TermDocumentMatrix(corpus)
 termFrequency <- rowSums(as.matrix(tdm))
 #termFrequency <- subset(termFrequency, termFrequency >= 10)
 qplot(seq(length(termFrequency)),sort(termFrequency), xlab = "index", ylab = "Freq")
 
 #Clustering
-#of words according to their co-occurrence in documents
-tdm2 <- removeSparseTerms(tdm, sparse=0.97)
-mat <- as.matrix(tdm2)
-distMatrix <- dist(mat)
-length(distMatrix)
-fit <- hclust(distMatrix, method="ward.D")
-plot(fit)
-
 
 #of of documents according to co-occurrence of terms
 dtm <- DocumentTermMatrix(corpus, control = list(weighting=weightTfIdf))
 mat <- as.matrix(dtm)
-k <- c(2)
+k <- c(2, 4, 8, 16)
 
+#nism ziher ce pravilno rise oz. ce je to zahtevano
 for(i in 1:length(k)){
   kmeansResult <- kmeans(mat, k[i])
-  
-  #plot(kmeansResult,data = mat, color = train$label)
-  #SNE-t
-  tsne.proj <- Rtsne(as.matrix(kmeansResult), perplexity=20, theta=0.2, dims=2, check_duplicates = F)
+  tsne.proj <- Rtsne(as.matrix(kmeansResult$cluster), perplexity=10, theta=0.2, dims=2, check_duplicates = F)
   df.tsne <-tsne.proj$Y
-  qplot(df.tsne[,1],df.tsne[,2],color = train$label)
+  #print(qplot(df.tsne[,1],df.tsne[,2], color = kmeansResult$cluster))
+  print(qplot(df.tsne[,1],df.tsne[,2], color = train$label))
 }
 
 #POS vector for each document
-s <- as.String(content(corpus))
+#vrne out of memmory error :/ 
+posvectors <- c()
+for(i in 1:length(corpus)){
+posvectors[[i]] <- c()
+s <- as.String(content(corpus[[i]]))
 a1 <- annotate(s, Maxent_Sent_Token_Annotator())
 a2 <- annotate(s, Maxent_Word_Token_Annotator(), a1)
 a3 <- annotate(s, Maxent_POS_Tag_Annotator(), a2)
 a3w <- subset(a3, type == "word")
 tags <- sapply(a3w$features, `[[`, "POS")
+print(tags)
+print(length(tags))
+posvectors[[i]] <- tags
+}
 
 # 3.MODELING
 #imbalanced
@@ -116,44 +125,8 @@ table(train$label)
 table(test$label)
 
 # Identify the class column
-test_corpus <- Corpus(VectorSource(test$text_a))
-test_corpus <- tm_map(test_corpus, removeWords, stopwords('english'))
-conn = file("english.stop.txt", open="r")
-mystopwords = readLines(conn)
-close(conn)
-conn = file("unknownWords.txt", open="r")
-unknown = readLines(conn)
-close(conn)
-test_corpus <- tm_map(test_corpus, removeWords, unknown)
-test_corpus <- tm_map(test_corpus, removeWords, mystopwords)
-test_corpus <- tm_map(test_corpus, removeNumbers)
-test_corpus <- tm_map(test_corpus, removePunctuation)
-test_corpus <- tm_map(test_corpus, stripWhitespace)
-
-#Anonymize proper nouns
-sent_ann <- Maxent_Sent_Token_Annotator()
-word_ann <- Maxent_Word_Token_Annotator() 
-person_ann <- Maxent_Entity_Annotator(kind = "person")
-location_ann <- Maxent_Entity_Annotator(kind = "location")
-organization_ann <- Maxent_Entity_Annotator(kind = "organization")
-
-entities <- function(annots, kind)
-{
-  k <- sapply(annots$features, `[[`, "kind")
-  s[annots[k == kind]]
-}
-
-for (i in 1:length(test_corpus)){
-  s <- as.String(content(test_corpus[[i]]))
-  if(nchar(trimws(s)) == 0) next
-  
-  ann <- annotate(s, list(sent_ann, word_ann, person_ann, location_ann, organization_ann))
-  test_corpus <- tm_map(test_corpus, removeWords, as.vector(entities(ann, "person")))
-  test_corpus <- tm_map(test_corpus, removeWords, as.vector(entities(ann, "location")))
-  test_corpus <- tm_map(test_corpus, removeWords, as.vector(entities(ann, "organization")))
-  content(test_corpus[[i]])
-}
-
+train_corpus <- clean_data(test)
+content(test_corpus[[1]])
 test_tdm <- TermDocumentMatrix(test_corpus)
 
 #
